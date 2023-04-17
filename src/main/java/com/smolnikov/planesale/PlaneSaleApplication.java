2020718eda4a51ed8cf7176f831e8344891ec36a
lexa2hk <alesha.smolnikoff@gmail.com>
2023-04-17 12:28:23 +0300

    Initial commit

---
+++ src/main/java/com/smolnikov/planesale/PlaneSaleApplication.java
@@ -0,0 +1,13 @@
+package com.smolnikov.planesale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaneSaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaneSaleApplication.class, args);
+    }
+
+}
